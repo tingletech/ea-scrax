fd48191ed59615b218a6f00c46fdc9a763852a32
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-03-31 16:07:29 -0700

    implimented editor iframe
    running run_my_transform() from the console works sort of... but
    I can't get the timing right, going to need to use defered or something

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,33 @@
 ;
 var global_debug = {};
-// magic globals for saxon-ce
-//  array to hold saxon errors
-var errors = [];
-//  fires when saxon is loaded
-var onSaxonLoad = function () {
 
-  var xml = Saxon.requestXML($('#xml').val());
-  var xslt = Saxon.requestXML($('#xslt').val());
 
-  errors = [];
+var run_my_transform = function () {
 
-  // Saxon.setLogLevel("FINE");
+  var errors = [];
+
+  var xml  = Saxon.parseXML($($( '#xml')[0].contentWindow.document).find('textarea').val()); 
+  var xslt = Saxon.requestXML('http://socialarchive.iath.virginia.edu/xtf/cpf2html/cpf2html.xsl');
+
   var proc = Saxon.newXSLT20Processor(xslt);
+  // Saxon.setLogLevel('FINE');
   Saxon.setErrorHandler(handler);
   var fragment = proc.transformToFragment(xml, document);
 
-  // http://stackoverflow.com/a/998325/1763984
-  var d = $("#ead-results")[0].contentWindow.document; // contentWindow works in IE7 and FF
-  d.open(); d.close(); // must open and close document object to start using it!
-
+  var d = $("#html")[0].contentWindow.document;
   $("body", d).append(fragment);
-
+  
   function handler(saxonError) {
     errors.push(saxonError.message + " " + saxonError.level + " " + saxonError.time);
   }
 
+  // http://tingletech.tumblr.com/post/81315608445/
+  function stringToXML(oString) {
+    //code for IE
+    if (window.ActiveXObject) { 
+      return (new ActiveXObject("Microsoft.XMLDOM")).loadXML(oString);
+    } else { // code for Chrome, Safari, Firefox, Opera, etc. 
+      return (new DOMParser()).parseFromString(oString, "text/xml");
+    }
+  }
 };
